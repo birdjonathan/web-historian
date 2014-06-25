114e22069666e840ea5bffc7bba65fd3996d22ae
Jonathan Bird <jonbird81@gmail.com>
2014-06-25 16:54:33 -0500

    Now answering get requests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,52 @@
 var path = require('path');
+var nodeStatic = require('node-static')
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+
+
+/* These headers will allow Cross-Origin Resource Sharing (CORS).
+ * This CRUCIAL code allows this server to talk to websites that
+ * are on different domains. (Your chat client is running from a url
+ * like file://your/chat/client/index.html, which is considered a
+ * different domain.) */
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
+var file = new (nodeStatic.Server)("./public", {});
+
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // res.end(archive.paths.list);
+
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain"
+
+  switch (req.method) {
+    case 'GET':
+      var statusCode = 200;
+      res.writeHead(statusCode, headers);
+      var filePath = 'public/index.html'
+
+      res.end(file.serve(req, res));
+
+      break;
+    case 'POST':
+      var message = '';
+      res.on('data', function(d) {
+        message += d;
+      });
+      res.on('end', function(d) {
+        callback(d);
+      });
+      var statusCode = 201;
+      req.data;
+      break;
+    default:
+      res.statusCode(404);
+      res.end();
+      break;
+  }
 };
